515d19d8a9aa542c6fbb7ff386710fffb28923f9
16gordona <16gordona@my.weston.org>
2016-04-18 19:22:26 -0400

    Create Deck-Archetype.py

---
+++ Deck-Archetype.py
@@ -0,0 +1,10 @@
+
+Synergies = ["Dragon" , "Beast" , "Pirate" , "Murloc" , "Silver Hand Recruit", "Mech"] #tribals
+Synergies = Synergies + ["Charge" , "Taunt" , "Windfury" , "Battlecry" , "Deathrattle" , "Inspire"] #Key words
+Synergies = Synergies + ["Healing" , "Secrets" , "Reno"] #miscellaneous
+
+Classes = ["Paladin" , "Warrior" , "Rogue" , "Druid" , "Hunter" , "Mage" , "Shaman" , "Warlock" , "Priest"]
+
+SynNum = 3 #number of synergies requested
+
+for i in range(SynNum):
